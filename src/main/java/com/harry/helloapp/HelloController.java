11483fcd11c625e7fe4962237d9a7bce6a16d5c5
harry-forge <harjinder2003.02@gmail.com>
2026-05-25 13:40:40 +0530

    update

--- src/main/java/com/harry/helloapp/HelloController.java
+++ src/main/java/com/harry/helloapp/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello(){
-        return "Hello Harjinder bhai......";
+        return "Hello Harjinder brother...";
     }
 
 }
